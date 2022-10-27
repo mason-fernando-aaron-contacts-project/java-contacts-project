7d0cbfaf639d923eb160f3ae46dfa2aa9e3ad439
Fernando Lopez <flopez00032@gmail.com>
2022-10-27 09:38:33 -0500

    created variables on contacts class

--- Contacts.java
+++ Contacts.java
@@ -1,3 +1,24 @@
 public class Contacts {
     // Comment for commit
+
+    // Contact phone number variable
+    protected int phoneNumber;
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    // Contact name variable
+    protected String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
