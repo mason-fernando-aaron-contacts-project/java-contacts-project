022b0c038db884aa17fffdf7a0bb13236cce4274
Aaron Morris <aaron.r.morris3@gmail.com>
2022-10-27 16:44:03 -0500

    merging-issues

--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -2,8 +2,9 @@ public class ContactsApp {
     public static void main(String[] args) {
         ContactsMethods test = new ContactsMethods();
 //        System.out.println(test.pathExists("contactsPackage"));
-        System.out.println(test.pathExists("contactsPackage/contactlist.txt"));
-        test.readFile("contactsPackage/contactlist.txt");
+//        System.out.println(test.pathExists("contactsPackage/contactlist.txt"));
+
+        test.contactsDisplay();
 
 
 
