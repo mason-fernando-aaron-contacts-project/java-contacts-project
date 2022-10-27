2e910ed11e64b5cd7f19c354b6c7bbf7c3b0bd32
Mason Wudtee <mason.wudtee@gmail.com>
2022-10-27 10:28:14 -0500

    created ContactsMethods Interface and some methods

---
+++ src/ContactsMethods.java
@@ -0,0 +1,15 @@
+public interface ContactsMethods {
+    // Added by Mason
+    public String getPath();
+
+    public void setPath();
+
+    public void createFile();
+
+    public boolean pathExists();
+
+    public void addContact();
+
+    public void deleteContact();
+    // Added by Mason
+}
