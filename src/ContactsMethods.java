ac49c4964f892174d14452ae3a45da1ac7ef972b
Aaron Morris <aaron.r.morris3@gmail.com>
2022-10-27 15:43:46 -0500

    commit-loop-complete

--- src/ContactsMethods.java
+++ src/ContactsMethods.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -6,10 +7,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactsMethods implements ContactsInterface {
-     public static void constactsDisplay() {
-//        Movie[] movies = MoviesArray.findAll();    // This will assign all the movies to the variable
-//        String userChoice = "timmmmmeh"; // We put timmehh because if  can equal to anything
-//        while (!userChoice.equals("0")){   // while the user choice does not equal to 0 it can do any of the options
+
+
+//    public ContactsMethods() {
+//    }
+
+    public void constactsDisplay() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Contacts> contactsList = new ArrayList<>();
+        String userChoice = "timmmmmmmmeee";
+
+        while (!userChoice.equals("5")) {
             System.out.println("What would you like to do");
             System.out.println("1 - View contacts.");
             System.out.println("2 - Add a new contact");
@@ -17,10 +25,60 @@ public class ContactsMethods implements ContactsInterface {
             System.out.println("4 - Delete an existing contact.");
             System.out.println("5 - Exit.");
             System.out.println("Enter an option (1, 2, 3, 4 or 5 ");
+            userChoice = scanner.next();
 
-//            userChoice = input.getString("Enter your choice:");
+            switch (userChoice) {
+                case "0" -> {
+                    System.out.println(0);
+                }
+                case "1" -> {
+                    System.out.println(1);
+                }
+                case "2" -> {
+                    System.out.println(2);
+                }
+                case "3" -> {
+                    System.out.println(3);
+                }
+                case "4" -> {
+                    System.out.println(4);
+                }
+                case "5" -> {
+                    System.out.println(5);
+                }
+                default -> {
+                    System.out.println("Wrong input you Dummy");
+                    constactsDisplay();
+                }
+            }
+        }
     }
 
+//            userChoice = input.getString("Enter your choice:");
+
+
+    Scanner scanner = new Scanner(System.in);
+
+    public String getString(){
+//            System.out.println("Enter your input:");
+        return scanner.nextLine();
+    }
+    public int getInt() {
+        System.out.println("Enter an integer:");
+//             get user input as a string
+        // parse the string into a number
+        // if the parse input throws an exception, recurse
+        // if no exception, return the parsed number
+
+        String userInput = getString();
+        try {
+            return Integer.valueOf(userInput);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            System.out.println("Incorrect input format. Please try again.");
+            return getInt();
+        }
+    }
     public ContactsMethods() {
     }
 
@@ -33,12 +91,15 @@ public class ContactsMethods implements ContactsInterface {
          return pathExist;
      }
 
-     public void printContacts(){
-         List<String> lines = new ArrayList<>();
-         try{
-             lines = Files.readAllLines()
-         }
-     }
+
+
+
+//     public void printContacts(){
+//         List<String> lines = new ArrayList<>();
+//         try{
+//             lines = Files.readAllLines()
+//         }
+//     }
 
 //        public void createPath(String directory) {
 //         Path dataDirectory = Paths.get(directory);
