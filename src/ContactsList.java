449b78f9447a1ff5f48e6c70166b182e1d3f5687
Fernando Lopez <flopez00032@gmail.com>
2022-10-31 09:27:06 -0500

    functionality finishe

--- src/ContactsList.java
+++ src/ContactsList.java
@@ -1,33 +1,33 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class ContactsList {
-
-
-    /*=============================== VARIABLES ================================*/
-    public static ArrayList<Contacts> contactsArrayList;
-
-    public static ArrayList<Contacts> getContactsArrayList() {
-        return contactsArrayList;
-    }
-
-    public static void setContactsArrayList(ArrayList<Contacts> contactsArrayList) {
-        ContactsList.contactsArrayList = contactsArrayList;
-    }
-
-
-    /*=============================== METHODS ================================*/
-
-    public static void addContacts(Contacts newContact){
-        contactsArrayList.add(newContact);
-    }
-
-    public static void addContacts(Contacts newContact,Contacts newContact2,Contacts newContact3){
-        contactsArrayList.addAll(List.of(newContact,newContact2,newContact3));
-    }
-
-    public ContactsList() {
-    }
-
-
-}// end of contacts list class
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class ContactsList {
+//
+//
+//    /*=============================== VARIABLES ================================*/
+//    public static ArrayList<Contacts> contactsArrayList;
+//
+//    public static ArrayList<Contacts> getContactsArrayList() {
+//        return contactsArrayList;
+//    }
+//
+//    public static void setContactsArrayList(ArrayList<Contacts> contactsArrayList) {
+//        ContactsList.contactsArrayList = contactsArrayList;
+//    }
+//
+//
+//    /*=============================== METHODS ================================*/
+//
+//    public static void addContacts(Contacts newContact){
+//        contactsArrayList.add(newContact);
+//    }
+//
+//    public static void addContacts(Contacts newContact,Contacts newContact2,Contacts newContact3){
+//        contactsArrayList.addAll(List.of(newContact,newContact2,newContact3));
+//    }
+//
+//    public ContactsList() {
+//    }
+//
+//
+//}// end of contacts list class
--- src/util/Input.java
+++ src/util/Input.java
@@ -3,112 +3,96 @@ package util;
 import java.util.Scanner;
 
 public class Input {
+    private Scanner scanner;
 
-//    shapes.Circle circle = new shapes.Circle();
-
-    private static Scanner scanner;
-
-
-      public Input (String prompt){
-        prompt = scanner.next();
-
-      }
-
-      public String getString(String string){
-//        System.out.println("Enter text:");
-        Integer.valueOf(string);
-        Double.valueOf(string);
-
-        System.out.println(string);
-        return scanner.nextLine();
-
-      }
-      public String getString (){
-        System.out.println("Enter text:");
-        return scanner.nextLine();
-      }
-
-      public boolean yesNo (){
-//        scanner.nextLine();
-        boolean yesLogic = true;
-
-          System.out.println("Would you like to go again? (y/n)");
-          String userInput = scanner.next().toLowerCase();
-          if (userInput.equals("y") || userInput.equals("yes")) {
-              return true;
-          } else {
-            yesLogic = false;
-            System.out.println("Later skater!");
-
-            return false;
-          }
-
-      }
-
-      public double yesNo2 (){
-
-        System.out.println("Continue? (y/n)");
-        String recursion = scanner.next().toLowerCase();
-        if (recursion.equals("y") || recursion.equals("yes")) {
-          return 1;
-        } else {
-          return 0;
-        }
-      }
-
-
-
-
-      int getInt (int min, int max){
-        System.out.printf("Enter a number between %d and %d%n", min, max);
-        int userInput = scanner.nextInt();
-        if (userInput >= min && userInput <= max){
-          return userInput;
-        } else {
-          getInt(min, max);
-
-        }
-        return 0;
-      }
-
-    double getDouble (double min, double max){
-      System.out.printf("Enter a number between %f and %f%n", min, max);
-      double userInput = scanner.nextDouble();
-      if (Math.ceil(userInput) >= min && Math.ceil(userInput) <= max){
-        System.out.println("Good Job!");
-        return userInput;
-      } else {
-        System.out.println("Your number sucks. Try again.");
-        getDouble(min, max);
-      }
-      return 0;
-    }
-
-    double getDouble () {
-      System.out.println("Enter a decimal number (double)");
-      double userDouble = scanner.nextDouble();
-      return userDouble;
-    }
-
-    int getInt (){
-      System.out.println("Enter a number");
-      int userInt = scanner.nextInt();
-        return userInt;
-    }
-
-      public Input (){
-        scanner = new Scanner(System.in);
-      }
-
-      // Getter
-      public Scanner getScanner(){
+    public Scanner getScanner() {
         return scanner;
-      }
+    }
 
-      // Setter
-      public void setScanner(Scanner scanner) {
+    public void setScanner(Scanner scanner){
         this.scanner = scanner;
-      }
+    }
 
 
-  }
+    public String getString(){
+        return scanner.nextLine();
+    }
+    public String getString(String prompt){
+        System.out.println(prompt);
+        return scanner.nextLine();
+    }
+
+    public boolean yesNo(){
+        System.out.println("yes or no?");
+        String userInput = scanner.next().toLowerCase();
+        return userInput.equalsIgnoreCase("y")||userInput.equalsIgnoreCase("yes");
+    }
+
+    public boolean yesNo(String prompt){
+        System.out.println(prompt);
+        System.out.println("yes or no?");
+        String userInput = scanner.next().toLowerCase();
+        return userInput.equalsIgnoreCase("y")||userInput.equalsIgnoreCase("yes");
+    }
+
+
+    public int getInt(){
+        System.out.println("Enter an Integer:");
+        return scanner.nextInt();
+    }
+
+    public int getInt(int min, int max){
+        System.out.printf("Enter a number from %d to %d %n", min,max);
+        int userInput = scanner.nextInt();
+
+        if(userInput < min | userInput > max){
+            System.out.printf("%d is not within the range bro%n",userInput);
+            return getInt(min,max);
+        } else{ return userInput;}
+    }
+    public int getInt(String prompt){
+        System.out.println(prompt);
+        int min = getInt();
+        int max = getInt();
+        System.out.printf("Your parameters are from %d to %d %n", min,max);
+        int userInput = scanner.nextInt();
+
+        if(userInput < min | userInput > max){
+            System.out.printf("%d is not within the range bro%n",userInput);
+            return getInt(min,max);
+        } else{ return userInput;}
+    }
+
+    public double getDouble(){
+       return scanner.nextDouble();
+    }
+
+    public double getDouble(String prompt){
+        System.out.println(prompt);
+       return scanner.nextDouble();
+    }
+
+
+    // recursive if the number is within the range
+    public double getDouble(double min, double max){
+        System.out.printf("Enter a number from %f to %f %n", min,max);
+        double userInput = scanner.nextDouble();
+
+        if(userInput < min || userInput > max){
+            return getDouble(min,max);
+        } else{ return userInput;}
+
+    }
+
+
+
+public Input(){
+        scanner = new Scanner(System.in);
+}
+
+
+
+
+
+
+}// end of Input class
