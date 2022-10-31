d5a16105b5b8266d8a9e954b3a9ba08db6249b29
Mason Wudtee <mason.wudtee@gmail.com>
2022-10-31 12:20:27 -0500

    cleaning code and adding comments

--- src/ApplicationMethods.java
+++ src/ApplicationMethods.java
@@ -11,64 +11,64 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ApplicationMethods {
-    static Input input = new Input();
+// =================================================== Variables =======================================================
+        static Input input = new Input();
         static Path p = Paths.get("src","contactsPackage", "contactlist.txt");
 
         static String toBeAdded;
 
-        // intermediate ArrayList of Strings
+        // intermediate ArrayList of Strings (tempArrayList)
         public static ArrayList<String> tempArrayList = new ArrayList<>();
         public static ArrayList<Contact> contactsList = new ArrayList<Contact>();
 
+// ==================================================== Methods ========================================================
 public void promptUser() {
         Scanner scanner = new Scanner(System.in);
         String userChoice = "whatever";
+                while (!userChoice.equals("6")) {
+                System.out.println("What would you like to do");
+                System.out.println("1 - View contacts.");
+                System.out.println("2 - Add a new contact");
+                System.out.println("3 - Search a contact by name.");
+                System.out.println("4 - Delete an existing contact.");
+                System.out.println("5 - Saving");
+                System.out.println("6 - Exit.");
+                System.out.println("Enter an option (1, 2, 3, 4, 5 or 6)");
+                userChoice = scanner.next();
 
-        while (!userChoice.equals("6")) {
-        System.out.println("What would you like to do");
-        System.out.println("1 - View contacts.");
-        System.out.println("2 - Add a new contact");
-        System.out.println("3 - Search a contact by name.");
-        System.out.println("4 - Delete an existing contact.");
-        System.out.println("5 - Saving");
-        System.out.println("6 - Exit.");
-        System.out.println("Enter an option (1, 2, 3, 4, 5 or 6)");
-        userChoice = scanner.next();
+                        switch (userChoice) {
+                                case "1" -> viewContacts();
+                                case "2" -> addToContactsList();
+                                case "3" -> searchContact();
+                                case "4" -> deleteContact();
+                                case "5" -> {
+                                        System.out.println("Saving");
+                                        saveLines(tempArrayList);
+                                }
+                                case "6" -> System.out.println("Goodbye");
 
-        switch (userChoice) {
-        case "1" -> viewContacts();
-        case "2" -> addToContactsList();
-        case "3" -> searchContact();
-        case "4" -> deleteContact();
-        case "5" -> {
-                System.out.println("Saving");
-                SaveLines(tempArrayList);
-
-        }
-        case "6" -> System.out.println("Goodbye");
-
-default -> {
-        System.out.println("Wrong input you Dummy");
-        promptUser();
-        }
-        }
-        }
-        }
+                                default -> {
+                                System.out.println("Wrong input you Dummy");
+                                promptUser();
+                                }
+                        } // End Switch
+                } // End While
+        } // End promptUser();
 
 public static void viewContacts (){
 
         List<String> lines = new ArrayList<>();
 
         try{
-        lines = Files.readAllLines(p);
+                lines = Files.readAllLines(p);
         } catch (IOException e) {
-        e.printStackTrace();
+                e.printStackTrace();
         }
 
         for(String contacts : lines){
         System.out.println(contacts);
-        }
-        }
+        } // End enhanced for-loop
+        } // End viewContacts();
 
 public static void addNewContact(){
         String userInput = input.getString("Enter Contact name and Phone Number");
@@ -79,11 +79,11 @@ public static void addNewContact(){
         } catch (IOException e) {
         e.printStackTrace();
         }
-        }
+        } // End addNewContact();
 
 public static void addToContactsList(){
         // this block makes an instance of a Contact
-//        String usrInput = input.getString("Contact Nickname");
+        //  String usrInput = input.getString("Contact Nickname");
         String name = input.getString("Contact name");
         String number = input.getString("Contact Number");
         number = formatPhoneNum(number);
@@ -113,11 +113,11 @@ public static void searchContact (){
         for(String contacts : lines){
         if(contacts.contains(userSearch))
         System.out.println("here you go: "+contacts);
-        }
+        } // End enhanced for-loop
         } catch (IOException e) {
         e.printStackTrace();
         }
-        }
+        } // End searchContact();
 
 public static void deleteContact(){
         String toBeDeleted = input.getString("What contact would you like to delete?");
@@ -127,13 +127,15 @@ public static void deleteContact(){
         updatedNames.add(name);
         }
         }
+        // updatedNames is written using the writeLines function
         writeLines(updatedNames);
         }
 
-private static void SaveLines(List<String> lines) {
+private static void saveLines(List<String> lines) {
         for(Contact contact: contactsList){
-//        System.out.println(contact.getName());
+        // name and number stored in toBeAdded variable
                 toBeAdded = contact.getName()+":"+contact.getNumber();
+                // toBeAdded added into tempArrayList
                 tempArrayList.add(toBeAdded);
         }
         // single string being added to the intermediate ArrayList of String to be able to write to the text file
@@ -142,7 +144,7 @@ private static void SaveLines(List<String> lines) {
         } catch (IOException e) {
                 throw new RuntimeException(e);
         }
-        }
+        } // End SaveLines
 
 private static List<String> readLines() {
         List<String> names;
@@ -171,4 +173,4 @@ private static List<String> readLines() {
                 return new StringBuilder().append(areaCode).append(firstThree).append(lastFour).toString();
         }
 
-}
+} // ApplicationMethods Class End
--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -1,13 +1,6 @@
 public class ContactsApp {
     public static void main(String[] args) {
-
-
     ApplicationMethods app = new ApplicationMethods();
     app.promptUser();
-
-
-
-
-
-    }// end of main
-}// end of class
+    }
+}
