cc9ced2eff7d00aa1f62c45074c57b19a7ed4f97
Aaron Morris <aaron.r.morris3@gmail.com>
2023-01-26 17:49:00 -0600

    HackerRank Problem 04

--- src/Hackerank.java
+++ src/Hackerank.java
@@ -17,6 +17,23 @@ public class Hackerank {
             System.out.println("Not Weird");
         }
 
+        import java.util.Scanner;
+
+        public class Solution {
+
+            public static void main(String[] args) {
+                Scanner scan = new Scanner(System.in);
+                int i = scan.nextInt();
+                double d = scan.nextDouble();
+                scan.nextLine();    /*Ghosting may occur needs this so rest of lines can be read*/
+                String s = scan.nextLine();
+
+                System.out.println("String: " + s);
+                System.out.println("Double: " + d);
+                System.out.println("Int: " + i);
+            }
+        }
+
 
     }
 }
